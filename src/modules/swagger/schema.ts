7e372564f7fae61c482a1289e7107997ead14d83
constROD <rialcantara21@gmail.com>
2023-04-26 01:02:26 +0800

    fix: validations duplicate and schema title

--- src/modules/swagger/schema.ts
+++ src/modules/swagger/schema.ts
@@ -1,13 +1,17 @@
 import { docs as samplesDocs } from 'modules/samples/functions/handler';
 import { docs as someExampleDocs } from 'modules/some-example/functions/handler';
 import { version } from 'package.json';
+import { SERVICE_NAME } from 'serverless/constants';
+import { transformToPascal } from 'shared/utils/commons';
 import swaggerJsdoc from 'swagger-jsdoc';
+
+const serviceName = transformToPascal(SERVICE_NAME);
 
 const options: swaggerJsdoc.OAS3Options = {
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'Sample API',
+      title: `${serviceName} API`,
       version,
     },
     paths: {
